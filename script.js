26390f83f93ef3569c586d6cbe8bd4351cf021de
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-05-17 18:23:29 +0200

    button update

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
 let nav = 0; //keeping track of which month it is
-let clicked = null; //whicever day has been clicked
-let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')): []; //JSON.parse because to make sure the object exists in local storage.
+let clicked = null; //whichever day has been clicked
+let events = localStorage.getItem("events")
+  ? JSON.parse(localStorage.getItem("events"))
+  : []; //JSON.parse because to make sure the object exists in local storage.
 // if it does not exist in local storage then return an empty array.
 
-
-const calendar = document.getElementById('calendar'); //reference for the calendar
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
+const calendar = document.getElementById("calendar"); //reference for the calendar
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 function load() {
- const dt = new Date();
+  const dt = new Date();
 
- const day = dt.getDate();
- const month = dt.getMonth();
- const year = dt.getFullYear();
+  if (nav !== 0) {
+    dt.setDate(1)
+    dt.setMonth(new Date().getMonth() + nav);
+  } //
 
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
 
- const firstDayOfMonth = new Date(year, month, 1);
- const daysInMonth = new Date(year, month + 1 , 0).getDate(); // will return the number of days in the month
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate(); // will return the number of days in the month
 
+  const dateString = firstDayOfMonth.toLocaleDateString("en-uk", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  const paddingDays = weekdays.indexOf(dateString.split(",")[0]);
 
+  document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
+    "en-uk",
+    { month: "long" }
+  )} ${year}`; // displaying the month and year ontop of calendar by referncing the monthDisplay id
 
- const dateString = firstDayOfMonth.toLocaleDateString('en-uk',{
+  calendar.innerHTML = ""; //wipes all squares/days when buttons are triggered to not create more squares than neccessary. sets html to empty string.
 
- weekday:'long',
- year: 'numeric',
- month:'numeric', 
- day:'numeric',
- });
- const paddingDays = weekdays.indexOf(dateString.split(',')[0]);
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement("div");
+    daySquare.classList.add("day");
 
- document.getElementById('monthDisplay').innerText = 
- `${dt.toLocaleDateString('en-uk',{month: 'long'})} ${year}`;// displaying the month and year ontop of calendar by referncing the monthDisplay id
-
-
- for(let i = 1; i <= paddingDays + daysInMonth; i++){
-    const daySquare = document.createElement('div');
-    daySquare.classList.add('day');
-    
-    if (i > paddingDays){
+    if (i > paddingDays) {
       daySquare.innerText = i - paddingDays;
 
-      daySquare.addEventListener('click',()=> console.log('click'));
+      daySquare.addEventListener("click", () => console.log("click"));
     } else {
-       daySquare.classList.add('padding');
+      daySquare.classList.add("padding");
     }
-   
-   calendar.appendChild(daySquare);
- }
+
+    calendar.appendChild(daySquare);
+  }
 }
 //months are counted from 0, which is why there is a + 1 to calc no. of months
+
+/**
+ * below creates a function with tthe working 'next' and 'back' buttons for calendar.
+ */
+function initButtons() {
+  document.getElementById("nextButton").addEventListener("click", () => {
+    nav++;
+    load();
+  });
+  document.getElementById("backButton").addEventListener("click", () => {
+    nav--;
+    load();
+  });
+}
+
 load();
